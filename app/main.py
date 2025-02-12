4e4a4edd123faedaf14a18f5a9f1d45057e45a0a
jelius47 <jeliusheneriko47@gmail.com>
2025-02-12 14:27:33 +0300

    added some new files for proper fast-api arrangement and added some CORS

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from .routers import todos, reminders, calender
 from .database import Base, engine
 
+from fastapi.middleware.cors import CORSMiddleware
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -9,6 +11,15 @@ app = FastAPI()
 app.include_router(todos.router)
 app.include_router(reminders.router)
 app.include_router(calender.router)
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/")
 def read_root():
